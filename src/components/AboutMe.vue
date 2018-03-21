21333422461eb239d25e75c4023104203b8c69d4
Irving Cabello <irvollo@gmail.com>
2018-03-20 19:12:38 -0600

    Responsive text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,19 +1,50 @@
 <template>
-    <div>
-        <v-card flat class="title">
-          <v-card-text>
-            <h1>
-                Hi, Irvollo is my name and Development is my game.
-            </h1>
-          </v-card-text>
-        </v-card>
-    </div>
+    <v-container class="titulo" fluid grid-list-xs>
+     <v-flex xs12>
+     <v-layout row justify-center>
+       <v-card raised>
+        <v-card-title primary-title>
+          <div>
+            <h1 class="headline mb-0"><b>Irvollo</b> is my name, <b>Development</b> is my game.</h1>
+            <h1 class="headline mb-0">I'm a Mexican Full-Stack Javascript Developer 🌮 </h1>
+          </div>
+        </v-card-title>
+      </v-card>
+     </v-layout> 
+    <br />
+    <v-layout row justify-center>
+      <v-btn color="primary" raised large>Available for Hire</v-btn>  
+    </v-layout>
+    </v-flex>
+     <v-flex xs12>
+       <img class="flatAvatar" src="../static/avatar.png" />
+     </v-flex>
+    </v-container>
 </template>
 <script>
     
 </script>
 <style>
-    .title {
-        margin-top: 15%;
-    }
+.titulo {
+    margin-top: 10%;
+    background: #FFFFFF;
+    height: 100%;
+}
+
+.flatAvatarContainer {
+    position: relative;
+    text-align: center;
+}
+
+.flatAvatar {
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    bottom: 0px;
+    min-width:300px;
+    max-width: 25%;
+}
+
 </style>
